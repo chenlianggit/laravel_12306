b505a235231613c16e3d0107a2f5ca7b498e3084
陈亮 <chenliang@aldwx.com>
2018-12-12 22:26:57 +0800

    init初始化

---
+++ app/Models/WxAccessToken.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenliang
+ * Date: 2018/11/21
+ * Time: 下午3:27
+ */
+
+namespace App\Models;
+
+
+class WxAccessToken extends Model
+{
+    public $table = 'wx_access_token';
+
+    public $primaryKey = 'id';
+
+}
---
+++ app/Support/helpers.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenliang
+ * Date: 2018/11/21
+ * Time: 下午2:39
+ */
+
+const OK    = 200;
+const ERROR = 0;
+
+function array2object($arr)
+{
+    $json = json_encode($arr);
+    return json_decode($json, false);
+}
+
+
+/**
+ * 输出json数据
+ * @param int $code
+ * @param string $message
+ * @param string $data
+ * @param string $noCache
+ */
+function outputToJson($code, $message, $data = [])
+{
+    header("Content-type: application/json; charset=utf-8");
+    $msg = array(
+        'code' => $code,
+        'msg' => $message,
+        'data' => $data,
+    );
+
+    $msg = json_encode($msg);
+
+    echo $msg;
+    exit();
+}
